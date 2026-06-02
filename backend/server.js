ef7a9ab09ba9e747621a27c43a0fef095378e5a3
lathavairamuthu1004-crypto <lathavairamuthu.1004@gmail.com>
2026-06-02 23:47:41 +0530

    images replaced

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,8 @@ app.use(cors({
   origin: [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
     "http://31.97.237.122:5007",
   ],
   credentials: true,
