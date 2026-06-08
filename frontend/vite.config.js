937657566955e72f67b175010ad31233f9fa2536
Chinnadurai <rchunna2003@example.com>
2026-06-08 15:11:33 +0530

    new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,5 +10,14 @@ export default defineConfig({
     },
     build: {
         outDir: 'build',
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return 'vendor';
+                    }
+                }
+            }
+        }
     },
 });
